716f575ff64014b3f9a08fdd028caad2984ce9ba
Jhonatamsoares <git@github.com:Jhonatamsoares/dsmeta.git>
2022-07-17 15:45:30 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,13 +1,27 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
-function NotificationButton() {
-    return(
-        <div className="dsmeta-red-btn">
+
+type props = {
+    saleId: number;
+
+}
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("SUCESSO");
+        }
+        )
+}
+function NotificationButton({ saleId }: props) {
+    return (
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
-        
+
         </div>
     )
-   
-  }
-  
-  export default NotificationButton;
+
+}
+
+export default NotificationButton;
